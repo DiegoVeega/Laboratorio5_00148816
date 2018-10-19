591e700b29aa1125ca6f099ffe67c97a0878e67a
diegoveega <00148816@uca.edu.sv>
2018-10-19 02:34:45 -0600

    commit

---
+++ asset/js/app.js
@@ -0,0 +1,80 @@
+
+const navSlide = ()=>{
+    const burger = document.querySelector(".burger"); 
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener("click", ()=>{
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) =>{
+            
+            if(link.style.animation)
+            {
+                link.style.animation = '';
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+0.3}s`;
+            }
+            
+        });
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
+/*********************************Colocar aca el desarrollo de su ejercicio***************************/
+var cont=1;
+var bitacoras=[];
+var formulario=documen.getElementById('bitacora');
+
+formulario.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+});
+
+bitacoras = {
+    cant,
+    fecha,
+    descripcion,
+    contidad
+}
+formulario.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    let bitacora = {
+    cant:cont,
+    fecha: formulario[1].value,
+    descripcion: formulario[2].value,
+    cantidad: formulario[3].value
+  }
+  bitacoras.push(bitacora);
+   cont++;
+   mostrar();
+ });
+
+ const crearElemento = (bitacora, tbody) =>{
+    let tr = document.createElement("tr");
+    Object.values(bitacora).forEach(item => {
+        let td = document.createElement("td");
+        let content=document.createTexNode(item);
+        td.appendChild(content);
+        tr.appendChild(td);
+        tr.setAttribute('class','click');
+    });
+    tbody.appendChild(tr);
+  }
+  const eliminar= (tbody)=>{
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+   }
+   const agregar= ()=>{
+    var eventtr = document.querySelectorAll(".click");
+      eventtr.forEach((item, index) => {
+      item.addEventListener("click", () => {
+      document.querySelector("#fecha").value = item.childNodes[0].textContent;
+      document.querySelector("#descp").value = item.childNodes[1].textContent;
+      document.querySelector("#cant").value = item.childNodes[2].textContent;
+     });
+    })
+   } 
